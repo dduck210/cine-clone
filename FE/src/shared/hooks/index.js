743606064b3756116dd1df07cbfaf2952a63142f
dduck210 <duonganhduc6a4@gmail.com>
2026-06-08 15:32:34 +0700

    chore: remove unused usePaymentPoller hook

--- FE/src/shared/hooks/index.js
+++ FE/src/shared/hooks/index.js
@@ -1,5 +1,4 @@
 export { default as usePagination }       from "./use-pagination";
 export { useAsync, useAsyncState }        from "./use-async";
-export { default as usePaymentPoller }    from "./use-payment-poller";
 export { default as useInView }           from "./use-in-view";
 export { usePushSubscription }            from "./use-push-subscription";
