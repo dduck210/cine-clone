ab37191b0196b516bf71371e9672ad8309152089
dduck210 <duonganhduc6a4@gmail.com>
2026-06-08 15:02:56 +0700

    fix: remove dead payment service functions pointing to non-existent routes

--- FE/src/api/services/payment-service.js
+++ FE/src/api/services/payment-service.js
@@ -6,17 +6,8 @@ export const getMomoStatus = (bookingId) =>
 export const getCassoStatus = (bookingId) =>
   axiosInstance.get(`/payments/casso/status/${bookingId}`).then((r) => r.data);
 
-export const getPaymentStatus = (orderId) =>
-  axiosInstance.get(`/payments/status/${orderId}`).then((r) => r.data);
-
 export const createMomoPayment = (bookingId) =>
   axiosInstance.post("/payments/momo/create", { bookingId }).then((r) => r.data);
-
-export const initMomoPayment = (orderId) =>
-  axiosInstance.post("/payments/momo/init", { orderId }).then((r) => r.data);
-
-export const initBankTransfer = (orderId) =>
-  axiosInstance.post("/payments/bank/init", { orderId }).then((r) => r.data);
 
 export const confirmMomoPayment = (params) =>
   axiosInstance.post("/payments/momo/confirm", params).then((r) => r.data);
